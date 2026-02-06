8bf3aa7b9778dc55b9c7df630dea3f48b886c877
Bernhard Schmitt <schmitt@sitegeist.de>
2026-02-06 12:42:10 +0100

    Add findChildren resource and the option to limit to properties

--- Classes/Domain/ContentRepositoryReadingElements.php
+++ Classes/Domain/ContentRepositoryReadingElements.php
@@ -15,7 +15,8 @@ use Neos\Neos\Domain\Service\ContentContextFactory;
 #[Flow\Scope('singleton')]
 class ContentRepositoryReadingElements
 {
-    public const FIND_DESCENDANTS_URI = 'contentsubgraph://find-descendants/{dimensionSpacePoint}/{ancestorNodeAggregateId}/{nodeTypeName}';
+    public const FIND_CHILDREN_URI = 'contentsubgraph://find-children/{dimensionSpacePoint}/{parentNodeAggregateId}/{nodeTypeName}/{limitToPropertyNames}';
+    public const FIND_DESCENDANTS_URI = 'contentsubgraph://find-descendants/{dimensionSpacePoint}/{ancestorNodeAggregateId}/{nodeTypeName}/{limitToPropertyNames}';
 
     public function __construct(
         protected ContentContextFactory $contentContextFactory,
@@ -25,18 +26,79 @@ class ContentRepositoryReadingElements
     }
 
     /**
-     * @param \stdClass<string,string> $dimensionSpacePoint
+     * @param list<string>|null $limitToPropertyNames
+     * @return array<string,mixed>
+     */
+    #[McpResourceTemplate(
+        uriTemplate: self::FIND_CHILDREN_URI,
+        name: 'find-children',
+        description: 'A list of all available child nodes of a given parent that are of a given type. This is a rather efficient query; use this if you already know the parent under which you want to search. To reduce response size, you can limit the returned properties to the list of given names in the parameter limitToPropertyNames',
+    )]
+    public function findChildren(
+        string $dimensionSpacePoint,
+        string $parentNodeAggregateId,
+        string $nodeTypeName,
+        string $limitToPropertyNames,
+    ): array {
+        $result = [];
+        $this->securityContext->withoutAuthorizationChecks(
+            function() use(
+                $nodeTypeName,
+                $dimensionSpacePoint,
+                $parentNodeAggregateId,
+                $limitToPropertyNames,
+                &$result,
+            ) {
+                $dimensionSpacePoint = \json_decode(\urldecode($dimensionSpacePoint), true, 512, JSON_THROW_ON_ERROR);
+                $nodeTypeName = \urldecode($nodeTypeName);
+                $limitToPropertyNames = $limitToPropertyNames === '*'
+                    ? null
+                    : \json_decode(\urldecode($limitToPropertyNames), true, 512, JSON_THROW_ON_ERROR);
+                $dimensions = [];
+                foreach ($dimensionSpacePoint as $dimensionName => $dimensionValue) {
+                    $dimensions[$dimensionName] = $this->contentDimensionPresetSource->getAllPresets()[$dimensionName]['presets'][$dimensionValue]['values'];
+                }
+                $contentContext = $this->contentContextFactory->create([
+                    'workspaceName' => 'user-admin',
+                    'dimensions' => $dimensions,
+                    'targetDimensions' => $dimensionSpacePoint,
+                    'invisibleContentShown' => true,
+                ]);
+
+                $ancestorNode = $contentContext->getNodeByIdentifier($parentNodeAggregateId);
+                $flowQuery = new FlowQuery([$ancestorNode]);
+                $nodes = [];
+                foreach ($flowQuery->children('[instanceof ' . $nodeTypeName . ']') as $node) {
+                    /** @var Node $node */
+                    $nodes[] = $this->serializeNode($node, $limitToPropertyNames);
+                }
+
+                $result = [
+                    'uri' => self::FIND_CHILDREN_URI,
+                    'name' => 'Child nodes',
+                    'description' => 'A list of nodes. The aggregateId identifies a node in the subgraph. The properties field contains the current state of the properties of that node. The nodeTypeName field contains the name of the type of that node, for more information see the ' . NodeTypeSchemaResource::URI . ' resource.',
+                    'mimeType' => 'application/json',
+                    'text' => \json_encode($nodes),
+                ];
+            }
+        );
+
+        return $result;
+    }
+
+    /**
      * @return array<string,mixed>
      */
     #[McpResourceTemplate(
         uriTemplate: self::FIND_DESCENDANTS_URI,
         name: 'find-descendants',
-        description: 'A list of all available descendant nodes of a given ancestor that are of a given type',
+        description: 'A list of all available descendant nodes of a given ancestor that are of a given type. This is a rather expensive query; use this if do not yet know the structure or the parent to search children of. To reduce response size, you can limit the returned properties to the list of given names in the parameter limitToPropertyNames',
     )]
     public function findDescendants(
         string $dimensionSpacePoint,
         string $ancestorNodeAggregateId,
         string $nodeTypeName,
+        string $limitToPropertyNames,
     ): array {
         $result = [];
         $this->securityContext->withoutAuthorizationChecks(
@@ -44,10 +106,14 @@ class ContentRepositoryReadingElements
                 $nodeTypeName,
                 $dimensionSpacePoint,
                 $ancestorNodeAggregateId,
+                $limitToPropertyNames,
                 &$result,
             ) {
                 $dimensionSpacePoint = \json_decode(\urldecode($dimensionSpacePoint), true, 512, JSON_THROW_ON_ERROR);
                 $nodeTypeName = \urldecode($nodeTypeName);
+                $limitToPropertyNames = $limitToPropertyNames === '*'
+                    ? null
+                    : \json_decode(\urldecode($limitToPropertyNames), true, 512, JSON_THROW_ON_ERROR);
                 $dimensions = [];
                 foreach ($dimensionSpacePoint as $dimensionName => $dimensionValue) {
                     $dimensions[$dimensionName] = $this->contentDimensionPresetSource->getAllPresets()[$dimensionName]['presets'][$dimensionValue]['values'];
@@ -64,11 +130,7 @@ class ContentRepositoryReadingElements
                 $nodes = [];
                 foreach ($flowQuery->find('[instanceof ' . $nodeTypeName . ']') as $node) {
                     /** @var Node $node */
-                    $nodes[] = [
-                        'aggregateId' => $node->getIdentifier(),
-                        'properties' => $node->getProperties(),
-                        'nodeTypeName' => $node->getNodeTypeName(),
-                    ];
+                    $nodes[] = $this->serializeNode($node, $limitToPropertyNames);
                 }
 
                 $result = [
@@ -82,5 +144,21 @@ class ContentRepositoryReadingElements
         );
 
         return $result;
+    }
+
+    private function serializeNode(Node $node, ?array $limitToPropertyNames): array
+    {
+        return [
+            'aggregateId' => $node->getIdentifier(),
+            'properties' => array_filter(
+                iterator_to_array($node->getProperties()),
+                fn (string $propertyName) => !$limitToPropertyNames || in_array(
+                        $propertyName,
+                        $limitToPropertyNames
+                    ),
+                ARRAY_FILTER_USE_KEY
+            ),
+            'nodeTypeName' => $node->getNodeTypeName(),
+        ];
     }
 }
